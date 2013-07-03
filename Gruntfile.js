5934a1b0eba64f5f6527a52fc248717681c5c670
Peter Ajtai <peter.ajtai@thinksolid.com>
2013-07-03 08:28:16 -0700

    initial structure

---
+++ Gruntfile.js
@@ -0,0 +1,21 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+    "use strict";
+    
+    // load all grunt tasks
+    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+    // Project configuration.
+    grunt.initConfig({
+
+        reveal: {
+
+            livereload: {
+                slides:"test/slides.jade"
+            }
+        }
+    });
+
+    grunt.registerTask("server", "Build and watch task", ["reveal:livereload"]);
+};
